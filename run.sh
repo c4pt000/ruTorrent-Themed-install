70fbd881ff0ffebd66702f3a3fb3f2968be12ca6
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-09-20 09:16:53 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /opt
-wget https://github.com/c4pt000/rtinst/raw/master/rtinst-master.zip
+curl -O https://github.com/c4pt000/rtinst/raw/master/rtinst-master.zip
 unzip rtinst-master.zip
