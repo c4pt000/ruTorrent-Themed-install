0f9930618561bb0d5b3c53d6c78e3f9ba14c625f
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-09-20 10:18:51 -0400

    Create install-themes.sh

---
+++ install-themes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo 'install themes'
+
+cd /var/www/rutorrent/plugins/theme/themes
+svn co https://github.com/ArtyumX/ruTorrent-Themes/trunk/Agent34
+svn co https://github.com/ArtyumX/ruTorrent-Themes/trunk/Agent46
+svn co https://github.com/ArtyumX/ruTorrent-Themes/trunk/OblivionBlue
+svn co https://github.com/ArtyumX/ruTorrent-Themes/trunk/MaterialDesign
+svn co https://github.com/ArtyumX/ruTorrent-Themes/trunk/FlatUI_Dark
+svn co https://github.com/ArtyumX/ruTorrent-Themes/trunk/FlatUI_Light
+svn co https://github.com/ArtyumX/ruTorrent-Themes/trunk/FlatUI_Material
+svn co https://github.com/ArtyumX/ruTorrent-Themes/trunk/club-QuickBox
+
